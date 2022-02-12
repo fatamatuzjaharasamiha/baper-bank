dcd5168210b04645f84e9d96228a5f37df5fdcbf
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-02-12 19:17:55 +0600

    final part

--- js/banking.js
+++ js/banking.js
@@ -1,25 +1,63 @@
 //Handle depopsite button event
-
 document.getElementById('deposit-button').addEventListener('click', function () {
     // kaj kre kina dekhar jnno console kre dekhbo
     // console.log('deposite button clicked');
 
     // 1st step:------get the amount deposited/////////////
     // button click kre input k dhre anar jnno
-    const depositInput = document.getElementById("deposit-input");
+    const depositInput = document.getElementById('deposit-input');
     //inputer vetrer amount dhorar jnno
-    const newDepositeAmountText = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositeAmountText);
-
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+    console.log(newDepositAmount);
+    // //update deposit total
     const depositTotal = document.getElementById('deposit-total');
-
     const previousDepositTxt = depositTotal.innerText;
     const previousDepositAmount = parseFloat(previousDepositTxt);
-
     const newDepositTotal = previousDepositAmount + newDepositAmount;
-
     depositTotal.innerText = newDepositTotal;
+    // // //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
 
     //clear the deposit input field 
     depositInput.value = '';
 })
+
+//handle withdraw with event handler
+//Handle depopsite button event
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // kaj kre kina dekhar jnno console kre dekhbo
+    // console.log('withdrawe button clicked');
+
+    // 1st step:------get the amount withdrawed/////////////
+    // button click kre input k dhre anar jnno
+    const withdrawInput = document.getElementById('withdraw-input');
+    //inputer vetrer amount dhorar jnno
+    const newwithdrawAmountText = withdrawInput.value;
+    const newwithdrawAmount = parseFloat(newwithdrawAmountText);
+    console.log(newwithdrawAmount);
+    // //update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previouswithdrawTxt = withdrawTotal.innerText;
+    const previouswithdrawAmount = parseFloat(previouswithdrawTxt);
+    const newwithdrawTotal = previouswithdrawAmount + newwithdrawAmount;
+    withdrawTotal.innerText = newwithdrawTotal;
+    // // //update account balance
+    const balanceTotal = document.getElementById('balance-total');
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newwithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    //clear the withdraw input field 
+    withdrawInput.value = '';
+})
+
+//handle withdraw with event handler
